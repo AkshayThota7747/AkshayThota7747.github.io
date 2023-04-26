29cd83a668c7c1b666c687f61eb375ffa4170294
Akshay Thota <akshay.thota@juspay.in>
2023-04-27 01:38:07 +0530

    updated code

---
+++ data.js
@@ -0,0 +1,110 @@
+const jsonData = {
+    'morning6' : {
+      'header': 'ఉదయం 6:15 నుండి 8:30 వరకు గెలిచే రంగు',
+      '1': {
+        'url': 'https://www.w3schools.com/howto/img_avatar2.png',
+        'text': 'Text 1'
+      },
+  
+      '2': {
+        'url': 'https://www.w3schools.com/howto/img_avatar2.png',
+        'text': 'Text 2'
+      },
+  
+      '3': {
+        'url': 'https://www.w3schools.com/howto/img_avatar2.png',
+        'text': 'Text 3'
+      }
+    },
+  
+    'morning8' : {
+      'header': 'ఉదయం 8:30 నుండి 10:45 వరకు గెలిచే రంగులు',
+      '1': {
+        'url': 'https://www.w3schools.com/howto/img_avatar2.png',
+        'text': 'Text 1'
+      },
+  
+      '2': {
+        'url': 'https://www.w3schools.com/howto/img_avatar2.png',
+        'text': 'Text 2'
+      },
+  
+      '3': {
+        'url': 'https://www.w3schools.com/howto/img_avatar2.png',
+        'text': 'Text 3'
+      }
+    },
+  
+    'morning10' : {
+      'header': 'ఉదయం 10:45 నుండి మధ్యాహ్నం 1 వరకు గెలిచే రంగులు',
+      '1': {
+        'url': 'https://www.w3schools.com/howto/img_avatar2.png',
+        'text': 'Text 1'
+      },
+  
+      '2': {
+        'url': 'https://www.w3schools.com/howto/img_avatar2.png',
+        'text': 'Text 2'
+      },
+  
+      '3': {
+        'url': 'https://www.w3schools.com/howto/img_avatar2.png',
+        'text': 'Text 3'
+      }
+    },
+  
+    'afternoon1' : {
+      'header': 'మధ్యాహ్నం 1 నుండి మధ్యాహ్నం 3:15 వరకు గెలిచే రంగులు',
+      '1': {
+        'url': 'https://www.w3schools.com/howto/img_avatar2.png',
+        'text': 'Text 1'
+      },
+  
+      '2': {
+        'url': 'https://www.w3schools.com/howto/img_avatar2.png',
+        'text': 'Text 2'
+      },
+  
+      '3': {
+        'url': 'https://www.w3schools.com/howto/img_avatar2.png',
+        'text': 'Text 3'
+      }
+    },
+  
+    'afternoon3' : {
+      'header': 'మధ్యాహ్నం 3:15 నుండి సాయంత్రం 5:30 వరకు గెలిచే రంగులు',
+      '1': {
+        'url': 'https://www.w3schools.com/howto/img_avatar2.png',
+        'text': 'Text 1'
+      },
+  
+      '2': {
+        'url': 'https://www.w3schools.com/howto/img_avatar2.png',
+        'text': 'Text 2'
+      },
+  
+      '3': {
+        'url': 'https://www.w3schools.com/howto/img_avatar2.png',
+        'text': 'Text 3'
+      }
+    },
+    'evening' : {
+      'header': 'సాయంత్రం 5:30pm నుండి గెలిచే రంగులు',
+      '1': {
+        'url': 'https://www.w3schools.com/howto/img_avatar2.png',
+        'text': 'Text 1'
+      },
+  
+      '2': {
+        'url': 'https://www.w3schools.com/howto/img_avatar2.png',
+        'text': 'Text 2'
+      },
+  
+      '3': {
+        'url': 'https://www.w3schools.com/howto/img_avatar2.png',
+        'text': 'Text 3'
+      }
+    }
+  
+  }
+  
---
+++ index.js
@@ -0,0 +1,96 @@
+function createElement(tagName, className) {
+    const result = document.createElement(tagName);
+    if (className !== undefined) result.className = className;
+    return result;
+  }
+  
+  function createCarousel (header, dataArray) {
+  
+    let currentIndex = 0;
+  
+    function updatePhotoIndex (no) {
+      if (no === 0) {
+        prev.style.display = 'none';
+      } else if (no === dataArray.length - 1) {
+        next.style.display = 'none';
+      } else {
+        prev.style.display = next.style.display = 'block';
+      }
+  
+      for (let idx = 0; idx < dataArray.length; idx++) {
+        if (no === idx) {
+          imgArray[idx].classList.add('show-slide');
+          dotArray[idx].classList.add('active');
+        } else {
+          imgArray[idx].classList.remove('show-slide');
+          dotArray[idx].classList.remove('active');
+        }
+      };
+    }
+  
+    const container = createElement('div', 'container');
+  
+    const slideshowContainer = createElement('div', 'slideshow-container'),
+      h4 = createElement('h4'),
+      prev = createElement('a', 'prev'),
+      next = createElement('a', 'next');
+  
+    h4.innerText = header;
+    prev.innerText = '❮';
+    next.innerText = '❯';
+  
+    prev.addEventListener('click', function () { updatePhotoIndex(--currentIndex); });
+    next.addEventListener('click', function () { updatePhotoIndex(++currentIndex); });
+  
+    const imgArray = dataArray.map(({url, text}, idx) => {
+      const mySlides = createElement('div', 'mySlides fade'),
+        img = createElement('img', 'pic'),
+        textElement = createElement('div', 'text');
+  
+      img.src = url;
+      textElement.innerText = text;
+  
+      mySlides.append(img, textElement);
+  
+      if (currentIndex === idx)
+        mySlides.classList.add('show-slide');
+  
+      return mySlides;
+    });
+  
+    const dotHolder = createElement('div', 'dot-holder'),
+      dotArray = dataArray.map((_, idx) => {
+      const dot = createElement('span', 'dot');
+      dot.addEventListener('click', function () {
+        currentIndex = idx;
+        updatePhotoIndex(currentIndex);
+      });
+  
+      return dot;
+    });
+  
+    slideshowContainer.append(h4, ...imgArray, prev, next);
+    dotHolder.append(...dotArray);
+    container.append(slideshowContainer, dotHolder);
+  
+    updatePhotoIndex(currentIndex);
+  
+    return container;
+  }
+  
+  const lastText = document.getElementById('last-text');
+  
+  Object.values(jsonData).forEach(item => {
+    const data = Object.keys(item).reduce((acc, key) => {
+      if (key !== 'header') acc.push(item[key]);
+      return acc;
+    }, []);
+  
+  
+    document.body.insertBefore(createCarousel(item.header, data), lastText);
+  
+    // document.body.append()
+  });
+  
+  
+  
